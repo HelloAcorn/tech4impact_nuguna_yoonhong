820cd7eec3b0503eed6a0001bcb633b1c14b7b0d
helloacorn <dbsghd3820@gmail.com>
2024-11-11 05:48:29 +0000

    feat : 의사분류기 추가

--- model_download.py
+++
@@ -1,7 +0,0 @@
-from huggingface_hub import snapshot_download
-
-# Model path on Hugging Face
-model_name = "defog/llama-3-sqlcoder-8b"
-
-# Download the model
-snapshot_download(repo_id=model_name)
